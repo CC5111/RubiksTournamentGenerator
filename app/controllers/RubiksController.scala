350b5bab99582f00b121da046c33b68dde647064
Milenko Tomic <miletomc@gmail.com>
2016-07-29 14:23:42 -0400

    index page

---
+++ app/controllers/RubiksController.scala
@@ -0,0 +1,35 @@
+package controllers
+
+import javax.inject._
+import akka.actor.ActorSystem
+import models.daos._
+import models.entities.Supplier
+import models.persistence.SlickTables.SuppliersTable
+import play.api.libs.json.{JsValue, Json, Writes}
+import play.api.libs.streams.ActorFlow
+import play.api.mvc._
+import scala.concurrent.{Future, ExecutionContext}
+
+import models.entities._
+
+import play.api.data._
+import play.api.data.Forms._
+import play.api.mvc._
+import scala.concurrent.{Future, ExecutionContext}
+import play.api.data._
+import play.api.data.Forms._
+
+
+import play.api.i18n.Messages.Implicits._
+import play.api.Play.current
+
+/**
+  * Created by milenkotomic on 29-07-16.
+  */
+
+@Singleton
+class RubiksController @Inject()()(implicit ec: ExecutionContext, system: ActorSystem, mat: akka.stream.Materializer) extends Controller {
+  def index = Action{implicit request =>
+    Ok(views.html.index())
+  }
+}
